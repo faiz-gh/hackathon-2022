3daff0287d1f813e4d80ad3b0fb346e35b25c57a
Faiz Ghanchi <87257423+faiz-gh@users.noreply.github.com>
2022-11-26 14:14:35 +0000

    improved the database schema and naming convention

--- db/hackathon.sql
+++ db/hackathon.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: db:3306
--- Generation Time: Oct 22, 2022 at 06:42 PM
+-- Generation Time: Nov 26, 2022 at 02:12 PM
 -- Server version: 8.0.31
 -- PHP Version: 8.0.19
 
@@ -28,17 +28,18 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `adminlogin` (
-  `adminID` int NOT NULL,
-  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL
+  `admin_ID` int NOT NULL,
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `signup_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `adminlogin`
 --
 
-INSERT INTO `adminlogin` (`adminID`, `email`, `password`) VALUES
-(1, 'faiz@123', 'faiz@123');
+INSERT INTO `adminlogin` (`admin_ID`, `username`, `password`, `signup_date`) VALUES
+(2, 'admin', 'admin@123', '2022-11-26 14:11:35');
 
 -- --------------------------------------------------------
 
@@ -47,28 +48,28 @@ INSERT INTO `adminlogin` (`adminID`, `email`, `password`) VALUES
 --
 
 CREATE TABLE `birthform` (
-  `birthformID` int NOT NULL,
-  `userID` int NOT NULL,
-  `regNo` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `regDate` datetime DEFAULT NULL,
+  `birth_form_ID` int NOT NULL,
+  `user_ID` int NOT NULL,
+  `registration_no` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `registration_date` datetime DEFAULT CURRENT_TIMESTAMP,
   `ward` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `zone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `gender` int DEFAULT NULL,
-  `birthDate` datetime DEFAULT NULL,
-  `birthPlace` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `fatherName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `motherName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `tempAddress` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `permAddress` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `birth_date` datetime DEFAULT NULL,
+  `birth_place` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `father_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `mother_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `temporary_address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `permanent_address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `remarks` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `affiliationCode` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `motherID` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `fatherID` varchar(2555) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `marriageCert` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `dischargeSlip` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `birthCertificate` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `birthStatus` int DEFAULT '0'
+  `affiliation_code` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `mother_identity_proof` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `father_identity_proof` varchar(2555) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `marriage_certificate` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `discharge_slip` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `birth_certificate` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `registration_status` int DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -78,29 +79,29 @@ CREATE TABLE `birthform` (
 --
 
 CREATE TABLE `deathform` (
-  `deathFormID` int NOT NULL,
-  `userID` int NOT NULL,
-  `regNo` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `regDate` datetime DEFAULT NULL,
+  `death_form_ID` int NOT NULL,
+  `user_ID` int NOT NULL,
+  `registration_no` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `registration_date` datetime DEFAULT CURRENT_TIMESTAMP,
   `ward` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `zone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `gender` int DEFAULT NULL,
-  `deathDate` datetime DEFAULT NULL,
-  `deathPlace` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `motherName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `fatherName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `tempAddress` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `permAddress` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `death_date` datetime DEFAULT NULL,
+  `death_place` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `mother_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `father_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `temporary_address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `permanent_address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `remarks` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `aadharCard` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `aadhar_card` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `noc` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `rationCard` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `delayedPerm` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `postMortem` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `deathDeclaration` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `deathCertificate` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `deathStatus` int DEFAULT '0'
+  `ration_card` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `delayed_registration_permission` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `post_mortem_report` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `statement_of_death_declaration` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `death_certificate` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `registration_status` int DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -110,12 +111,12 @@ CREATE TABLE `deathform` (
 --
 
 CREATE TABLE `userlogin` (
-  `userID` int NOT NULL,
-  `firstName` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `lastName` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `user_ID` int NOT NULL,
+  `first_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `last_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `signup_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -126,30 +127,30 @@ CREATE TABLE `userlogin` (
 -- Indexes for table `adminlogin`
 --
 ALTER TABLE `adminlogin`
-  ADD PRIMARY KEY (`adminID`),
-  ADD UNIQUE KEY `email` (`email`);
+  ADD PRIMARY KEY (`admin_ID`),
+  ADD UNIQUE KEY `email` (`username`);
 
 --
 -- Indexes for table `birthform`
 --
 ALTER TABLE `birthform`
-  ADD PRIMARY KEY (`birthformID`),
-  ADD UNIQUE KEY `regNo` (`regNo`),
-  ADD KEY `userID` (`userID`);
+  ADD PRIMARY KEY (`birth_form_ID`),
+  ADD UNIQUE KEY `regNo` (`registration_no`),
+  ADD KEY `userID` (`user_ID`);
 
 --
 -- Indexes for table `deathform`
 --
 ALTER TABLE `deathform`
-  ADD PRIMARY KEY (`deathFormID`),
-  ADD UNIQUE KEY `regNo` (`regNo`),
-  ADD KEY `userID` (`userID`);
+  ADD PRIMARY KEY (`death_form_ID`),
+  ADD UNIQUE KEY `regNo` (`registration_no`),
+  ADD KEY `userID` (`user_ID`);
 
 --
 -- Indexes for table `userlogin`
 --
 ALTER TABLE `userlogin`
-  ADD PRIMARY KEY (`userID`),
+  ADD PRIMARY KEY (`user_ID`),
   ADD UNIQUE KEY `emaill` (`email`);
 
 --
@@ -160,25 +161,25 @@ ALTER TABLE `userlogin`
 -- AUTO_INCREMENT for table `adminlogin`
 --
 ALTER TABLE `adminlogin`
-  MODIFY `adminID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `admin_ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `birthform`
 --
 ALTER TABLE `birthform`
-  MODIFY `birthformID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `birth_form_ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- AUTO_INCREMENT for table `deathform`
 --
 ALTER TABLE `deathform`
-  MODIFY `deathFormID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `death_form_ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `userlogin`
 --
 ALTER TABLE `userlogin`
-  MODIFY `userID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `user_ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- Constraints for dumped tables
@@ -188,13 +189,13 @@ ALTER TABLE `userlogin`
 -- Constraints for table `birthform`
 --
 ALTER TABLE `birthform`
-  ADD CONSTRAINT `birthform_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `userlogin` (`userID`);
+  ADD CONSTRAINT `birthform_ibfk_1` FOREIGN KEY (`user_ID`) REFERENCES `userlogin` (`user_ID`);
 
 --
 -- Constraints for table `deathform`
 --
 ALTER TABLE `deathform`
-  ADD CONSTRAINT `deathform_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `userlogin` (`userID`);
+  ADD CONSTRAINT `deathform_ibfk_1` FOREIGN KEY (`user_ID`) REFERENCES `userlogin` (`user_ID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
